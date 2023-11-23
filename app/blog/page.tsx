5a007f807bd7ac19407388d404b8d3106e3be02a
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2023-11-23 15:00:05 +0100

    demo logo and Navbar (#1)

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./blog.module.scss";
+
+const BlogPage = () => {
+  return (
+    <div className={styles.main}>
+      <h1>Blog Page</h1>
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ app/components/Navbar/Hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./Hero.module.scss";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <div className={styles["hero-main"]}>
+      <div className={styles.container}>
+        {" "}
+        <div className={styles["hero-image"]}>
+          {" "}
+          <Image
+            src="/hexdia.png"
+            alt="hexdia logo"
+            width={1440}
+            height={350}
+            // layout="responsive"
+          />
+        </div>
+        <div className={styles["hero-text-wrapper"]}>
+          <section className={styles["hero-title-container"]}>
+            <h1 className={styles.title}>
+              Precision Diamond Drilling Solutions for Every Project
+            </h1>
+          </section>
+          <section className={styles["hero-text-container"]}>
+            <p className={styles.description}>
+              HEXDIA is a leading provider of diamond drilling operatives and
+              equipment, offering innovative solutions for construction,
+              demolition, and engineering projects. Our cutting-edge tools and
+              expert team ensure precision and efficiency in tackling the
+              toughest materials.
+            </p>
+            <section className={styles["hero-btn-container"]}>
+              <Link href="/blog">
+                <button className={styles["hero-btn"]}>Learn More</button>
+              </Link>
+              <Link href="/contact">
+                <button className={styles["hero-btn"]}>Contact Us</button>
+              </Link>
+            </section>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
